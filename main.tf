939a33691b4686421d4a609f0ddd7a5718a86928
Abhishek <abhi@example.com>
2025-07-06 18:07:37 +0530

    new code added

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "ThirdTryRG" {
     name = "firsttry"
     location = "Central US"
 }
-resource "azurerm_resource_group" "SecondTryRG" {
-    name = "secondtry"
-    location = "Central US"
-}
+# resource "azurerm_resource_group" "SecondTryRG" {
+#     name = "secondtry"
+#     location = "Central US"
+# }
