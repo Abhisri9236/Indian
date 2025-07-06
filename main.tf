ca61746da4ec5bc1e61aadbdf35e7a1f75be18fc
Telecom <cloud.sanjaysingh@gmail.com>
2025-07-06 19:07:01 +0530

    mumbaivala

--- main.tf
+++ main.tf
@@ -16,3 +16,8 @@ resource "azurerm_resource_group" "SecondTryRG1111" {
     name = "secondtry1111"
     location = "Central US"
 }
+
+resource "azurerm_resource_group" "oves" {
+    name = "secondtry1111mumbai"
+    location = "Central US"
+}
