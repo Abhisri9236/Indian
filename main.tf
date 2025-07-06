b6c94564733bcea05bd8902a3036508994e98734
Cloudsanjay <cloud.sanjaysingh@gmail.com>
2025-07-06 18:37:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "SecondTryRG1" {
     name = "secondtry1"
     location = "Central US"
 }
+
+resource "azurerm_resource_group" "SecondTryRG1111" {
+    name = "secondtry1111"
+    location = "Central US"
+}
