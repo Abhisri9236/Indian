f34eb11a9d8b6557cf55f24c2271fe602e29c436
rajat-sharma8181 <rajats12120@gmail.com>
2025-07-06 18:31:12 +0530

    rg12 added

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "SecondTryRG1" {
     name = "secondtry1"
     location = "Central US"
 }
+resource "azurerm_resource_group" "SecondTryRG12" {
+    name = "secondtry12"
+    location = "Central US"
+}
