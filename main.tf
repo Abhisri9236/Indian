f1a9742402138e19a583282de04ce983e0b0097e
rajat-sharma8181 <rajats12120@gmail.com>
2025-07-06 18:22:46 +0530

    second RG added

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "ThirdTryRG" {
     name = "firsttry"
     location = "Central US"
 }
-# resource "azurerm_resource_group" "SecondTryRG" {
-#     name = "secondtry"
-#     location = "Central US"
-# }
+resource "azurerm_resource_group" "SecondTryRG1" {
+    name = "secondtry1"
+    location = "Central US"
+}
